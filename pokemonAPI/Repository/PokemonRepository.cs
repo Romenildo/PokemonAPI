fc2559cdecfa4b7f575b6c7553ef72d2a8d3e0fb
Romenildo Ferreira <romenildoferreira@gmaill.com>
2023-07-03 22:31:25 -0300

    implementacao inicial do repository do pokemon

--- pokemonAPI/Repository/PokemonRepository.cs
+++ pokemonAPI/Repository/PokemonRepository.cs
@@ -11,9 +11,39 @@ namespace pokemonAPI.Repository
         {
             _context = context;
         }
+
+        public Pokemon GetPokemon(int id)
+        {
+            return _context.Pokemon.Where(p => p.Id == id).FirstOrDefault();
+        }
+
+        public Pokemon GetPokemon(string name)
+        {
+            return _context.Pokemon.Where(p => p.Name == name).FirstOrDefault();
+        }
+
+        public decimal GetPokemonRating(int pokeId)
+        {
+            //pegar todas as review quais possui o pokemon passado como id
+            var reviews = _context.Reviews.Where(r => r.Pokemon.Id == pokeId);
+
+            if(reviews.Count() > 0)
+            {
+                return 0;
+            }
+            //Fazer a media de todas as notas das review do pokemon
+            return ((decimal)reviews.Sum(r => r.Rating) / reviews.Count());
+
+        }
+
         public ICollection<Pokemon> GetPokemons()
         {
             return _context.Pokemon.OrderBy(p => p.Id).ToList();
+        }
+
+        public bool PokemonExists(int pokeId)
+        {
+            return _context.Pokemon.Any(p => p.Id == pokeId);
         }
     }
 }
